8a778d08f575a8ba01021d35c684639ead8c6113
Juan Agüí <agui@tid.es>
2014-06-30 15:29:11 +0200

    removes message box so that it can be used from WP background agents

--- src/NLog/AssemblyBuildInfo.cs
+++ src/NLog/AssemblyBuildInfo.cs
@@ -2,4 +2,4 @@
 // based on the version number saved in 'D:\Work\NLog2\src\NLog\..\..\NLog.version'
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.0.1")]
